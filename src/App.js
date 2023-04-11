d67bfc6afdc8ca834fbc54a5a7c7a398d8965d92
zeusmoney <ezechukwuchibuikemoses@gmail.com>
2023-04-11 13:23:01 +0100

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // Importing required dependencies
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 // Importing styles
 import './App.css';
@@ -8,6 +9,16 @@ function App() {
   // Component rendering
   return (
     <>
+      {/* Navbar component */}
+      <Navbar />
+
+      {/* Setting up routing using react-router-dom */}
+      <Routes>
+        {/* Setting up routes for each page */}
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </>
   );
 }
