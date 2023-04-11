2f08fe550e78319c1a2435275a8b42a284cc2e55
zeusmoney <ezechukwuchibuikemoses@gmail.com>
2023-04-11 20:08:33 +0100

    Created Rocket structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import './App.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>React redux group project</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
---
+++ src/Components/RocketLists.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RocketLists.css';
+
+function RocketLists() {
+  return (
+    <div className="rocket-container">
+      <span className="rocket-name">Falcon 9</span>
+    </div>
+  );
+}
+
+export default RocketLists;
