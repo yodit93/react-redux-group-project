1ba0a7f0ba3b86c66ada2fdd51dd5e25d82acbff
yodit <yoditabebe9391gmail.com>
2023-04-12 13:04:04 +0300

    Display rockets

--- src/Pages/Rockets.js
+++ src/Pages/Rockets.js
@@ -1,15 +1,19 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import RocketList from '../Components/RocketLists';
 import { fetchRockets } from '../Redux/Rockets/rocketsSlice';
 
 const Rockets = () => {
+  const { rockets } = useSelector((store) => store.rockets);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchRockets());
   }, [dispatch]);
   return (
     <ul className="rockets">
-      rockets
+      {rockets.map((rocket) => (
+        <RocketList key={rocket.id} rocket={rocket} />
+      ))}
     </ul>
   );
 };
