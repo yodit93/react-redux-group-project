e072d6aec04f43a9549c520c09193699252242a6
zeusmoney <ezechukwuchibuikemoses@gmail.com>
2023-04-13 14:54:42 +0100

    mission display

--- src/Pages/Missions.js
+++ src/Pages/Missions.js
@@ -1,9 +1,39 @@
 /* eslint-disable */
 /* stylelint-disable */
-import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchMissions } from '../Redux/Missions/missionsSlice';
+import MissionList from '../Components/MissionLists';
 
-function Missions() {
-  return <div>Missions</div>;
-}
+const Missions = () => {
+  const { missions, isLoading, error } = useSelector((store) => store.missions);
+  console.log('missions data', missions);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+  return (
+    <>
+      {isLoading && <h2>Loadding...</h2>}
+      {error && <h2>{error}</h2>}
+      <table className="missions">
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Join</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map((mission) => (
+            <MissionList key={mission.id} mission={mission} />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 export default Missions;
+
